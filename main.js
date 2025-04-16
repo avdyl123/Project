45d543417a8cf2e56fca9df256e1588b8552407c
avdyl123 <avdylzeqiri732@gmail.com>
2025-04-16 03:10:53 -0700

    a
    a

--- main.js
+++ main.js
@@ -34,6 +34,9 @@ closeModal.addEventListener('click', () => {
     modal.style.display = 'none';
 });
 
+
+
+
 prevBtn.addEventListener('click', () => {
     currentIndex = (currentIndex === 0) ? images.length - 1 : currentIndex - 1;
     fullscreenImage.src = images[currentIndex].src;
@@ -49,3 +52,47 @@ modal.addEventListener('click', (e) => {
         modal.style.display = 'none';
     }
 });
+
+
+
+function validation() {
+    var name = document.getElementById("name").value.trim();
+    var age = document.getElementById("age").value.trim();
+    var city = document.getElementById("city").value.trim();
+
+    var valid_name_regex = /^[A-Za-z]+$/;
+    var valid_age_regex = /^[0-9]+$/;
+
+    var isValid = true;
+
+    
+    if (!valid_name_regex.test(name)) {
+        document.getElementById("name_error").style.visibility = "visible";
+        document.getElementById("name").style.borderColor = "red";
+        isValid = false;
+    } else {
+        document.getElementById("name_error").style.visibility = "hidden";
+        document.getElementById("name").style.borderColor = "black";
+    }
+
+   
+    if (!valid_age_regex.test(age)) {
+        document.getElementById("age_error").style.visibility = "visible";
+        document.getElementById("age").style.borderColor = "red";
+        isValid = false;
+    } else {
+        document.getElementById("age_error").style.visibility = "hidden";
+        document.getElementById("age").style.borderColor = "black";
+    }
+
+    
+    if (city === "") {
+        document.getElementById("city_error").style.visibility = "visible";
+        document.getElementById("city").style.borderColor = "red";
+        isValid = false;
+    } else {
+        document.getElementById("city_error").style.visibility = "hidden";
+        document.getElementById("city").style.borderColor = "black";
+    }
+
+    return isValid;}
